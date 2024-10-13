54f5c565ae827e3e9c8c6690790e8cf47e3c9743
Sai Chittella <saichittella30@gmail.com>
2024-10-12 18:17:13 -0700

    Homepage Completed

--- vitalyte-website/src/app/home/page.tsx
+++ vitalyte-website/src/app/home/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
 			<div className="min-h-screen flex items-center justify-center bg-white">
 				<div className="flex lg:flex-row lg:items-start w-full">
 					<div className="lg:w-1/2 space-y-6 m-auto px-20">
-						<h1 className="text-6xl text-center text-[#005eb4] leading-tight">
+						<h1 className="text-6xl text-center text-[#005eb4] leading-tight tracking-wider font-bold">
 							Innovative Technology. <br />
 							Empowering Healthcare.
 						</h1>
-						<p className="text-center text-2xl text-gray-800">
+						<p className="text-center text-2xl text-gray-800 tracking-wider w-3/4 left-10 relative">
 							Meet{" "}
 							<span className="font-bold text-[#007698]">
 								Vitalyte
@@ -28,14 +28,14 @@ export default function Home() {
 							<span className="font-bold text-[#007698]">AI</span>
 							.
 						</p>
-						<button className="text-center items-center justify-center left-20 bg-[#007698] text-white py-4 px-10 rounded-full text-lg font-medium mt-4 hover:bg-blue-700 transition">
+						<button className="text-center items-center justify-center left-20 relative bg-[#007698] text-white py-4 px-10 rounded-full text-lg font-medium mt-4 hover:bg-blue-700 transition">
 							Learn about us &rarr;
 						</button>
 					</div>
 
 					{/* Change this later so that on smaller screens the img becomes the background */}
 					<div
-						className="mt-10 lg:mt-6 left-16 relative w-[100vw] h-[100vh] bg-cover bg-center"
+						className="mt-12 top-1 left-16 relative w-[100vw] h-[100vh] bg-cover bg-center"
 						style={{ backgroundImage: "url(/imgs/home_logo.svg)" }}
 					/>
 				</div>
