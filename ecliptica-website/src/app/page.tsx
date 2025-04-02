eb37502b62eb9ca40deff8801c6ef39db0da3683
Anika Samuel <anikasarah44@gmail.com>
2025-04-02 01:05:53 -0700

    Initial commit of FINAL Ecliptica project

---
+++ ecliptica-website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import HomePage from "./home/page";
+
+export default function Home() {
+	return (
+		<>
+			<HomePage />
+		</>
+	);
+}
