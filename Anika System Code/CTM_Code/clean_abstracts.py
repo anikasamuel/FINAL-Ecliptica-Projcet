f6fd9e90ca5e98b1eed60eb24d59b877f13e353a
Anika Samuel <anikasarah44@gmail.com>
2025-04-02 00:31:40 -0700

    Move all submodules into Anika System Code

--- Anika System Code/CTM_Code/clean_abstracts.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def remove_empty_abstracts(input_file, output_file):
-    df = pd.read_excel(input_file)  # or pd.read_excel if Excel
-    df = df[df['Abstract'].notna()]
-    df = df[df['Abstract'].str.strip() != '']
-    df.to_csv(output_file, index=False)
-    return output_file
